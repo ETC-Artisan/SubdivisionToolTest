2c4d80108a73aa5810ae627d2b9996b07de38bc8
Whitebamboo <xiunany@andrew.cmu.edu>
2022-09-15 10:58:39 -0400

    upload

---
+++ Source/HoudiniStartKitTest/HoudiniStartKitTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HoudiniStartKitTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOUDINISTARTKITTEST_API AHoudiniStartKitTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
